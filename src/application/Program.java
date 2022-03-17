e527b2e8adf43dcdf6d21f647f705144b342a61e
Gualdina Ramos Tavares Diogo <gualdinartd@ext.inditex.com>
2022-03-17 11:32:48 +0000

    projeto

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import boardgame.Position;
+import chess.ChessMatch;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		ChessMatch chessMatch = new ChessMatch();
+//receber as peças da partida
+		UI.printBoard(chessMacth.getPieces());
+	}
+
+}
---
+++ src/application/UI.java
@@ -0,0 +1,29 @@
+package application;
+//user interface
+public class UI {
+
+	public static void PrintBoard(ChessPieces[][] pieces) {
+		//impressao do tabuleiro
+		for( int i= 0; i<pieces.length; i++ ) {
+			System.out.print((8-i) + " ");
+			for(int j =0; i<pieces.length; j++) {
+				printPiece(pieces[i][j]);
+			}
+			//quebra de linha
+			System.out.println();
+		}
+		System.out.println(" a b c d e f g h ");
+	}
+	
+	//impressao de uma peca
+	private static void printPiece(ChessPiece piece) {
+		if(piece == null) {
+			System.out.print("-");
+		}else {
+			
+			System.out.print(piece);
+		}
+			System.out.print("");
+		}
+	}
+
